f179c80b82b9b42a06fb48c466d046e69f2eaa36
Olyazarichna <olya.zarichna@gmail.com>
2022-07-13 22:11:15 +0200

    save in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,31 @@ import Filter from 'components/Filter/Filter';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+  componentDidMount() {
+    let contacts = localStorage.getItem('contacts');
+    let parseContacts = JSON.parse(contacts);
+    if (contacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   haveContacts = (contacts, data) => {
-    return contacts.some(contact => contact.name.toLowerCase() === data.name.toLowerCase());
+    return contacts.some(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
   };
 
   formSubmitHandler = data => {
